2cfab15093404404f68dc5cb76c6b514c9d0541a
vinukumbhar <vinayak.intcon@gmail.com>
2024-12-03 18:03:02 +0530

    3-12

---
+++ clientSidebar/index.js
@@ -0,0 +1,23 @@
+
+
+const express = require('express');
+const cors = require('cors');
+const sidebarDataRoutes = require('./routes/sidebarDataRoutes');
+require("dotenv").config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes for main sidebar
+app.use('/api', sidebarDataRoutes);
+
+const connectToDatabase = require("../clientSidebar/config/db");
+
+const dbConStatus = connectToDatabase();
+
+const PORT = process.env.PORT || 9500;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
