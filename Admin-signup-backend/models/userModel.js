a9852d9ad2d544273157c1bc39d6d8f01f84ce30
vinukumbhar <vinayak.intcon@gmail.com>
2025-03-02 08:35:15 +0530

    package name in user model

--- Admin-signup-backend/models/userModel.js
+++ Admin-signup-backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 
