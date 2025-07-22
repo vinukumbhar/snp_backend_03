1ad829ec05028bcafe9f643ccd0b22b279d4fe91
vinukumbhar <vinayak.intcon@gmail.com>
2025-07-22 18:31:05 +0530

    internalchat

--- internal-communication/index.js
+++ internal-communication/index.js
@@ -6,7 +6,13 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true'); // Set the header to 'true' to allow credentials
+    next();
+  });
 
 
 const internalChatRoutes = require("./routes/internalChatRoutes");
